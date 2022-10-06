7f2058f8f8b18c21326e1b86c6d75036894c47cf
Omar Badawi <itsomar278@gmail.com>
2022-10-06 07:16:33 +0300

    function return type changed to use it in the SP

--- Req6.sql
+++ Req6.sql
@@ -1,15 +1,21 @@
-CREATE Function fnCompanions(@EpisodeId INT)
-RETURNS @Companion TABLE 
-(
-CompanionName VARCHAR(255)
-)
-As
+-- functions were drop and recreated cause return type isnt the same so we cannot alter them 
+-- i hate dealing with string in sql :)
+DROP FUNCTION fnCompanions ;
+
+CREATE FUNCTION fnCompanions(@EpisodeId INT)
+RETURNS VARCHAR(MAX)
+AS
 BEGIN
-    INSERT INTO @Companion
-     SELECT CompanionName 
+Declare @output AS VARCHAR(MAX)
+     SELECT @output = CONCAT( 'Companion No ' ,
+	                          CAST(ROW_NUMBER() OVER(order by C.CompanionId) AS varchar ) 
+							  , ' : '
+							 , C.CompanionName 
+							 , ' , '
+							 , @output)
 	 FROM tblCompanion C
 	 INNER JOIN tblEpisodeCompanion AS EC
 	 ON c.CompanionId = EC.CompanionId
 	 WHERE @EpisodeId = EC.EpisodeId
-RETURN; 
+RETURN @Output; 
 END;
